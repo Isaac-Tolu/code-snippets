93f784c0da6476b56254fa8d6b55bdf9c6151b0a
Isaac-Tolu <isaactoluwani30@gmail.com>
2021-10-26 17:46:51 +0100

    Chore: Code Maintenance

--- matrix.py
+++ matrix.py
@@ -1,11 +1,25 @@
 class Matrix:
-    def __init__(self, matrix_string):
+    """
+    Representation of a matrix's rows and columns.
+    Source: Exercism
+    """
+
+    def __init__(self, matrix_string: str):
+        """
+        Initialises matrix attribute
+        matrix_string format: '1 3 1\n4 5 6\n3 1 2'
+        """
         self.matrix = [[int(j) for j in i.split()] for i in matrix_string.split('\n')]
-    def row(self, index):
+
+    def row(self, index: int) -> list:
+        """Returns the indexed row"""
         return self.matrix[index - 1]
-    def column(self, index):
+
+    def column(self, index) -> list:
+        """Return the indexed column"""
         return list(list(zip(*self.matrix))[index - 1])
 
     # A lesson on code readability
-    def column2(self, index):
+    def column2(self, index) -> list:
+        """Returns the indexed column"""
         return [r[index - 1] for r in self.matrix]    
--- raindrops.py
+++ raindrops.py
@@ -1,4 +1,12 @@
 def convert(number: int) -> str:
+    """
+    Converts a number to raindrops..
+    If it is a factor of 3, 5 or 7.
+
+    Returns the number if it is not a factor.
+    """
+
     sounds = [(3, "Pling"), (5, "Plang"), (7, "Plong")]
     result = [sound for divisor, sound in sounds if number % divisor == 0]
+
     return "".join(result) or f"{number}"
