d15a217978b1eac0f50c74c2ee80fbc297a396ba
Isaac-Tolu <isaactoluwani30@gmail.com>
2021-11-01 19:20:40 +0100

    Feature: Work on the last testcase

--- swapdict.py
+++ swapdict.py
@@ -1,12 +1,33 @@
-def swapdict(dict_):
+def swapdict(dict_: dict) -> dict:
+    """
+    Swaps the keys and values of a dictionary.
+    Returns a new dictionary.
+
+    - If any of the values of dict_ is unhashable i.e list or dict,
+      the key-value pair would be ignored.
+
+    - If multiple keys in dict_ have the same value,
+      the keys would be a list in new_dict i.e
+      {'1': 1, '2': 2, '3': 3, '4': 2} -> {1: '1', 2: ['2', '4'], 3: '3'}
+    """
     
     new_dict = {}
+
     for i, j in dict_.items():
-        if (type(j) == list) or type(j) == dict:
+        if (type(j) == list) or (type(j) == dict):
             continue
-        new_dict[j] = i
+
+        check = new_dict.get(j)
+
+        if check:
+            if type(check) != list:
+                new_dict[j] = [check, i]
+            else:
+                check.append(i)
+                new_dict[j] = check
+        else:
+            new_dict[j] = i
 
     return new_dict
 
-# TESTCASE: Same values for different keys
-print(swapdict({'1': 1, '2': 2, '3': 2}))
+# Consider dict.setdefault
